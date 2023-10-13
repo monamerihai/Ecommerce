f175dc8420adb4e0eca75ce6dae4c39e96c0b396
Dheeraj Kumar <dheeraj@rediansoftware.com>
2023-10-13 17:51:54 +0530

    projects

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\subcategory;
 class WebsiteController extends Controller
 {
-    function shop(){
-        return view('website.shop');
+    function shop(Request $request, $id){
+        $sub = subcategory::where('id','$request->id')->get();
+        return view('website.shop', compact('subcategory'));
     }
 
 }
